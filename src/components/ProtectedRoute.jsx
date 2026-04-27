70fb7790e5f49c35b4b85dc55c9282e4829a4abf
Gaelle <gaelle013@hotmail.com>
2026-04-27 18:53:22 +0300

    added an admin account to be able to access the website

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -2,12 +2,22 @@ import { useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from '../firebase'
+import { getAdminUser } from '../adminAuth'
 
 function ProtectedRoute({ children }) {
   const [user, setUser] = useState(null)
   const [isCheckingAuth, setIsCheckingAuth] = useState(true)
 
   useEffect(() => {
+    // Check for admin user first
+    const adminUser = getAdminUser()
+    if (adminUser) {
+      setUser(adminUser)
+      setIsCheckingAuth(false)
+      return
+    }
+
+    // Otherwise check Firebase auth
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser)
       setIsCheckingAuth(false)
